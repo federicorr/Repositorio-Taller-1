a34506d68bb0a5cd71d6c2b8b24ddb340afa81e7
jcsanchezr9 <123523499+jcsanchezr9@users.noreply.github.com>
2023-02-11 18:53:02 -0500

    Update Taller 1.R

--- Taller 1.R
+++ Taller 1.R
@@ -16,3 +16,30 @@ import("https://github.com/ignaciomsarmiento/datasets/blob/main/GEIH_sample1.Rds
 
 urlbase <- paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/", 1:10, ".html")
 
+#Otra opción (Juan Camilo)
+
+library(pacman)
+p_load(readr, rvest, tidyverse, dplyr)
+
+urls <- c("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html",
+          "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_2.html",
+          "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_3.html",
+          "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_4.html",
+          "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_5.html",
+          "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_6.html",
+          "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_7.html",
+          "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_8.html",
+          "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_9.html",
+          "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_10.html")
+
+chunks <- list()
+
+for (url in urls) {
+  chunk <- read_html(url) %>%
+    html_table() %>%
+    as.data.frame()
+  chunks <- append(chunks, list(chunk))
+}
+
+df <- dplyr::bind_rows(chunks)
+
