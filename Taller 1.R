cba3b60a510c5a4f32a936e4ec252aff9d928dd0
federicorr <f.ramirezr@uniandes.edu.co>
2023-02-12 17:15:51 -0500

    actualización

--- Taller 1.R
+++ Taller 1.R
@@ -52,3 +52,13 @@ sum(is.na(df18))
 
 # Chequeamos los NAs de la base
 sapply(df18, function(x) sum(is.na(x))
+       
+#5A
+#Training(70%) y testing (30%)
+
+id_train <- smaple (1:nrow(db2), size = 0.7*nrow(db2), replace =F) 
+train <- db2["id_train"]
+
+id_test <- smaple (1:nrow(db2), size = 0.3*nrow(db2), replace =F) 
+test <- db2["id_test"]
+
